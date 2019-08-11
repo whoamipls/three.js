92e764743490f1fffe3d9acd64be9ecfa28f0e29
whoamipls <49585734+whoamipls@users.noreply.github.com>
2019-08-12 00:20:20 +0800

    Add files via upload

---
+++ examples/test_starfield/js/config.js
@@ -0,0 +1,29 @@
+let g_Systems = [
+    { name: '人工智能', value: '../datasets/blocks1.json' },
+    { name: '区块链', value: '../datasets/blocks2.json' },
+    { name: '光电空间' },
+    { name: '海洋科技' },
+    { name: '新能源' },
+    { name: '核电' },
+    { name: '生命与健康' },
+    { name: '先进材料' },
+    { name: '生态环境' },
+    { name: '化学键能' },
+    { name: '人造光源' },
+    { name: '量子通讯' },
+    { name: '超级光栅' }
+]
+
+let g_Filters = [
+    { name: '论文', value: 0, bgColor: 'rgb(200, 57, 229)' },
+    { name: '资讯', value: 1, bgColor: 'rgb(255, 87, 34)' },
+    { name: '标准', value: 2, bgColor: 'rgb(33, 150, 243)' },
+    { name: '报告', value: 3, bgColor: 'rgb(103, 58, 183)' },
+    { name: '主题', value: 4, bgColor: 'rgb(77, 185, 179)' },
+    { name: '学者', value: 4, bgColor: 'rgb(255, 152, 0)' },
+    { name: '机构', value: 4, bgColor: 'rgb(139, 195, 74)' },
+    { name: '期刊', value: 4, bgColor: 'rgb(233, 30, 99)' },
+    { name: '专利', value: 4, bgColor: 'rgb(3, 169, 244)' },
+    { name: '项目', value: 4, bgColor: 'rgb(255, 193, 7)' },
+    { name: '会议', value: 4, bgColor: 'rgb(174, 0, 8)' }
+]
